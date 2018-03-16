c96ac0f512718abce933a68715eb5cbcb0280746
zeth48 <shalam.zeth48@gmail.com>
2018-03-17 04:28:00 +0530

    Added window with postioning.

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 var electron = require('electron');
 var BrowserWindow = electron.BrowserWindow;
 var path = require('path');
-const {app, menu, Tray} = require('electron');
+const {app, Menu, Tray, MenuItem} = require('electron');
+
+const WIN_LEN = 300;
+const WIN_WID = 200;
+
+var window = null;
+var tray = null;
 //No frameless API for macOS https://github.com/electron/electron/blob/master/docs/api/frameless-window.md
 //For more option check https://github.com/electron/electron/blob/master/docs/api/browser-window.md
+//Use for Linux as the position is set according to linux.
 function createWindow(){
 	window = new BrowserWindow({
-		width: 600,
-		height: 400,
+		width: WIN_LEN,		
+		height: WIN_WID,
 		transparent: true,
 		frame: false,			//removes title bar --> Frameless on Ubuntu
 		show: false				//set to false for dock
@@ -15,11 +22,20 @@ function createWindow(){
 	window.loadURL('file://'+__dirname+'/index.html');
 	window.on('close', function(){
 		window = null;
-	})
+	});
 	window.on('blur', function(){
    		window.hide();			//Window hides when you click somewhere else
- 	})
+ 	});
  	const iconPath = path.join(__dirname, 'img/bitcoin.png');
  	tray = new Tray(iconPath);
+ 	tray.on('click', (event) => {
+ 		var screen = electron.screen;		//Gets info about the display
+    	const cursorPosition = screen.getCursorScreenPoint();
+	    const primarySize = screen.getPrimaryDisplay().workAreaSize;  
+	    window.setPosition(cursorPosition.x - (WIN_LEN/2),  cursorPosition.y);
+		window.isVisible() ? window.hide() : window.show();
+ 	});
+
 }
 app.on('ready', createWindow);
+	
